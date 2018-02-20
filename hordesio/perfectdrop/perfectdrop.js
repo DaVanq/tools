1eaaf487b286a7a412b1f6961499a5814f53efd4
LegusX <loghenrie@gmail.com>
2018-02-20 11:28:11 -0600

    fixing bugs

--- hordesio/perfectdrop/perfectdrop.js
+++ hordesio/perfectdrop/perfectdrop.js
@@ -17,6 +17,7 @@ window.onload = function(){
 			document.getElementById("item-selector").children[0].remove()
 		}
 		for (var i in stuff.name) {
+			if (typeof stuff.lvl[i] === "undefined") return
 			let option = document.createElement("option")
 			option.textContent = stuff.name[i]
 			document.getElementById("item-selector").appendChild(option)
@@ -30,7 +31,8 @@ window.onload = function(){
 		let chance = 1
 		for (var i in statNames) {
 			let stats = equip.stats[statNames[i]]
-			chance*=Math.round((stats.float)?((level*(stats.multi*stats.high)+stats.base)-(level*(stats.multi*stats.low)+stats.base)).toFixed(1)/0.1:Math.ceil((level*(stats.multi*stats.high)+stats.base)-(level*(stats.multi*stats.low)+stats.base)))
+			if (typeof stats.base === "undefined") chance*=Math.round((stats.float)?((level*(stats.multi*stats.high))-(level*(stats.multi*stats.low))).toFixed(1)/0.1:Math.ceil((level*(stats.multi*stats.high))-(level*(stats.multi*stats.low))))
+			else chance*=Math.round((stats.float)?((level*(stats.multi*stats.high)+stats.base)-(level*(stats.multi*stats.low)+stats.base)).toFixed(1)/0.1:Math.ceil((level*(stats.multi*stats.high)+stats.base)-(level*(stats.multi*stats.low)+stats.base)))
 		}
 		if (chance === 0) chance = 1
 		document.getElementById("result-text").textContent = "There is a 1 in "+chance+" chance that the next "+e.target.value+" you pick up will have perfect stats."
